ef44e6e089fca08f67a62cf67078fc477b1e7c30
zanedr <zanedanielrussell@gmail.com>
2022-05-27 12:21:27 -0600

    fix token select dropdown

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import ShareModal from "lit-share-modal-v3";
 
 function App() {
+  const [showShareModal, setShowShareModal] = useState(false)
+
+  useEffect(() => {
+    console.log('check showShareModal')
+  }, [showShareModal])
+
+  const conditionsCreated = (acc) => {
+    console.log('check acc', acc)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setShowShareModal(!showShareModal)}>
+          share modal
+        </button>
+        {showShareModal && (
+          <div className={'share-modal'}>
+            <ShareModal
+              onClose={() => {
+                setShowShareModal(false);
+                // setPermanent(true);
+                // setOpenCreateDraftOrderModal(true);
+              }}
+              onUnifiedAccessControlConditionsSelected={async (restriction) => {
+                conditionsCreated(restriction)
+              }}
+            />
+          </div>
+        )}
       </header>
     </div>
   );
