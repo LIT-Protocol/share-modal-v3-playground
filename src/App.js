328eaa96a7ece84cf247e46e2d15219d5dfb85a1
zanedr <zanedanielrussell@gmail.com>
2022-06-01 16:10:49 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -4,35 +4,52 @@ import ShareModal from "lit-share-modal-v3";
 
 function App() {
   const [showShareModal, setShowShareModal] = useState(false)
+  const [darkMode, setDarkMode] = useState(false)
+  const [devMode, setDevMode] = useState(false)
+
+  const [unifiedAccessControlConditions, setUnifiedAccessControlConditions] = useState('')
 
   useEffect(() => {
     console.log('check showShareModal')
   }, [showShareModal])
 
-  const conditionsCreated = (acc) => {
-    console.log('check acc', acc)
+  const onUnifiedAccessControlConditionsSelected = (shareModalOutput) => {
+    console.log('check acc', shareModalOutput)
+    setShowShareModal(false);
+    if (shareModalOutput.unifiedAccessControlConditions) {
+      const prettify = JSON.stringify(shareModalOutput.unifiedAccessControlConditions, undefined, 4);
+      setUnifiedAccessControlConditions(prettify)
+    }
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={() => setShowShareModal(!showShareModal)}>
-          share modal
+          show share modal
         </button>
+        <button onClick={() => setDarkMode(!darkMode)}>
+          dark mode: {darkMode ? 'on' : 'off'}
+        </button>
+        {/*<button onClick={() => setDevMode(!devMode)}>*/}
+        {/*  dev mode: {devMode ? 'on' : 'off'}*/}
+        {/*</button>*/}
         {showShareModal && (
           <div className={'share-modal'}>
             <ShareModal
+              allowDevMode={devMode}
+              darkTheme={darkMode}
               onClose={() => {
                 setShowShareModal(false);
-                // setPermanent(true);
-                // setOpenCreateDraftOrderModal(true);
               }}
-              onUnifiedAccessControlConditionsSelected={async (restriction) => {
-                conditionsCreated(restriction)
-              }}
+              onUnifiedAccessControlConditionsSelected={onUnifiedAccessControlConditionsSelected}
             />
           </div>
         )}
+        <div className={'lsm-dev-mode-container'}>
+          <label className={'lsm-dev-mode-container-label'}>Raw Access Control Conditions</label>
+          <textarea className={'lsm-dev-mode-textarea'} rows={35} value={unifiedAccessControlConditions} onChange={(e) => setUnifiedAccessControlConditions(e.target.value)}/>
+        </div>
       </header>
     </div>
   );
